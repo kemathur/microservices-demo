286befe487cf147200a7a138a6225bffbe0957e1
kemathur <keshavmathur@outlook.com>
2019-07-24 13:44:34 -0700

    try to remove color

--- deploy/docker-compose/launch.sh
+++ deploy/docker-compose/launch.sh
@@ -1,2 +1,3 @@
-docker-compose -f deploy/docker-compose/docker-compose.yml -f deploy/docker-compose/docker-compose.logging.yml -f deploy/docker-compose/docker-compose.monitoring.yml up -d
+docker-compose -f deploy/docker-compose/docker-compose.yml -f deploy/docker-compose/docker-compose.logging.yml up -d --no-color
+docker-compose -f deploy/docker-compose/docker-compose.monitoring.yml up -d --no-color
 scope launch
